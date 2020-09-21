491ae8ddf1bdf840cf1e7e2842767cef7d87108b
Mohammad Yousefnejad <Mohammad.Yousefnejad@MahanAir.Aero>
2020-09-21 17:19:00 +0330

    first project

---
+++ src/01-countdown-timer/script.js
@@ -0,0 +1,30 @@
+var bodyCtx = JSVs(),
+    newYears = new Date(new Date().getFullYear() + 1, 0, 1);
+
+setInterval(countdown, 1000), countdown();
+
+function countdown() {
+    var diff = (newYears - new Date()) / 1000,
+        days = Math.floor(diff / 3600 / 24),
+        hours = Math.floor((diff - days * 3600 * 24) / 3600),
+        minutes = Math.floor((diff - days * 3600 * 24 - hours * 3600) / 60),
+        seconds = Math.floor(diff - days * 3600 * 24 - hours * 3600 - minutes * 60);
+    bodyCtx.days.textContent = days;
+    bodyCtx.hours.textContent = hours;
+    bodyCtx.minutes.textContent = minutes;
+    bodyCtx.seconds.textContent = seconds;
+}
+
+
+
+function JSVs() {
+    return mount(document.body, {});
+    function mount(element, context) {
+        return mnt(element), context;
+        function mnt(el) { me(el); for (var i = 0; i < el.children.length; i++) mnt(el.children[i]); }
+        function me(e) {
+            var idAttr = e.attributes['id'];
+            idAttr && !context[idAttr.value] && (context[idAttr.value] = e);
+        }
+    }
+}
